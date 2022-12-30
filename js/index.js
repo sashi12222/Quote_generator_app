554078b335ced0365cc48790269b607be140a9ee
sashi12222 <sashiraj1222@gmail.com>
2022-12-30 17:44:56 +0530

    done

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const quoteText=document.querySelector(".quote"),
+authorName=document.querySelector(".author .name"),
+quoteBtn=document.querySelector("button"),
+soundBtn=document.querySelector(".sound"),
+copyBtn=document.querySelector(".copy"),
+twitterBtn=document.querySelector(".twitter");
+function randomQuote(){
+    quoteBtn.classList.add("loading")
+    quoteBtn.innerText="Loading Quote....."
+   fetch("https://api.quotable.io/random")
+   .then(res=>res.json())
+   .then(result=>{
+    console.log(result)
+    quoteText.innerHTML=result.content;
+    authorName.innerHTML=result.author;
+    quoteBtn.innerText="New Quote"
+    quoteBtn.classList.remove("loading")
+
+   })
+}
+
+soundBtn.addEventListener("click",()=>{
+    let utterance=new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText} `)
+    speechSynthesis.speak(utterance);
+});
+
+copyBtn.addEventListener("click",()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener("click",()=>{
+let tweetUrl=`https://twitter.com/intent/tweet?url=${quoteText.innerText}`
+window.open(tweetUrl,"_blank");
+})
+
+quoteBtn.addEventListener("click",randomQuote)
